90289968b66865e3b9da0e6b441783e7f60f3805
SyedHussain <tah94mid@gmail.com>
2021-06-09 03:27:06 -0400

    first commit

---
+++ src/sample/Main.java
@@ -0,0 +1,58 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+
+public class Main extends Application{
+
+    Button login;
+    Button registration;
+    Stage window;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        window = primaryStage;
+
+        window.setTitle("SecureNotePad");
+
+        Registration reg = new Registration();
+
+        Label label = new Label("Welcome to SecureNotePad");
+
+        login = new Button();
+        login.setText("Login");
+
+//        login.setOnAction(event -> t.print("1"));
+
+        registration = new Button();
+        registration.setText("Registration");
+        registration.setOnAction(event -> reg.register(window));
+
+        VBox layout = new VBox(20);
+        layout.getChildren().addAll(label, login, registration);
+        layout.setAlignment(Pos.CENTER);
+
+        Scene scene = new Scene(layout, 300, 250);
+        window.setScene(scene);
+        window.show();
+
+    }
+
+}
---
+++ src/sample/Registration.java
@@ -0,0 +1,140 @@
+package sample;
+
+import javafx.css.Match;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.geometry.*;
+
+import java.util.jar.Attributes;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Registration {
+
+    public void register(Stage regStage) {
+        Main main = new Main();
+
+//        regStage.initModality(Modality.APPLICATION_MODAL); //Block other input events until this is taken care off
+        regStage.setTitle("Registration");
+        regStage.setMinWidth(300);
+
+
+        GridPane grid = new GridPane();
+        grid.setPadding(new Insets(10, 10, 10, 10));
+        grid.setVgap(8);
+        grid.setHgap(10);
+
+        Label label = new Label();
+        label.setText("Please fill up all the boxes.");
+        GridPane.setConstraints(label, 1, 0);
+
+        //First Name label
+        Label fnameLabel = new Label("First Name");
+        GridPane.setConstraints(fnameLabel, 0, 1);
+
+        //First Name input
+        TextField fnameInput = new TextField();
+        fnameInput.setPromptText("First Name");
+        GridPane.setConstraints(fnameInput, 1, 1);
+
+        //Last Name label
+        Label lnameLabel = new Label("Last Name");
+        GridPane.setConstraints(lnameLabel, 0, 2);
+
+        //Name input
+        TextField lnameInput = new TextField();
+        lnameInput.setPromptText("Last Name");
+        GridPane.setConstraints(lnameInput, 1, 2);
+
+        //email label
+        Label emailLabel = new Label("Email");
+        GridPane.setConstraints(emailLabel, 0, 3);
+
+        //email input
+        TextField emailInput = new TextField();
+        emailInput.setPromptText("abc@abc.abc");
+        GridPane.setConstraints(emailInput, 1, 3);
+
+        //Pass label
+        Label passLabel = new Label("Password");
+        GridPane.setConstraints(passLabel, 0, 4);
+
+        //Pass input
+        TextField passInput = new TextField();
+        passInput.setPromptText("Exactly 4 digit");
+        GridPane.setConstraints(passInput, 1, 4);
+
+        Button signUpBtn = new Button("Sign Up");
+        GridPane.setConstraints(signUpBtn, 1, 6);
+        signUpBtn.setOnAction(e -> {
+            if (isName(fnameInput, lnameInput) && isEmail(emailInput) && isPassword(passInput)) {
+                // save the values in database
+                System.out.println("complete");
+
+            }
+        });
+
+
+        Button backBtn = new Button("Back to Main");
+        GridPane.setConstraints(backBtn, 1, 7);
+        backBtn.setOnAction(e -> {
+            try {
+                main.start(regStage);
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+        });
+
+
+        grid.getChildren().addAll(label, fnameLabel, fnameInput, lnameLabel, lnameInput, emailLabel, emailInput, passLabel, passInput, signUpBtn, backBtn);
+
+        Scene scene = new Scene(grid, 300, 250);
+        regStage.setScene(scene);
+        regStage.show();
+    }
+
+    public boolean isName(TextField fname, TextField lname){
+        String regex = "^[A-Za-z]\\w{2,29}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher fmatcher = pattern.matcher(fname.getText());
+        Matcher lmatcher = pattern.matcher(lname.getText());
+        if(fmatcher.matches() && lmatcher.matches()) {
+            return true;
+        }
+        else {
+            AlertBox.display("Incorrect Name!", "Please type in both the first and last name. " +
+                    "Minimum 3 characters and only letters are allowed! Please try again.");
+            return false;
+        }
+    }
+
+    public boolean isEmail(TextField email){
+        String regex = "^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(email.getText());
+        if(matcher.matches()) {
+            return true;
+        }
+        else {
+            AlertBox.display("Incorrect Email!", "Email address has to be in the form of abc@abc.abc! Please try again.");
+            return false;
+        }
+    }
+
+    public boolean isPassword(TextField password){
+        String regex = "\\d{4}";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(password.getText());
+        if (matcher.matches()) {
+            return true;
+        }
+        else {
+            AlertBox.display("Incorrect Password!", "Password has to be exactly 4 digit! Please try again.");
+            return false;
+        }
+    }
+
+}
+
